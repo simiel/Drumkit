dd422bdfaadc3164e0c38010509e0965a4b8c4c7
Simiek <samensah036@st.ug.edu.gh>
2022-11-09 12:27:00 +0000

    Finished drum kit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-window.addEventListener('keydown', function(e){
-    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
+
+
+function doit (e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return
+    audio.currentTime = 0
     audio.play()
-    console.log(audio)
-})
+    key.classList.add("playing")
+}
+
+window.addEventListener('keydown', doit)
+
+function removeTransition(e){
+    if(e.propertyName!="transform") return
+    this.classList.remove("playing")
+}
+
+const keys = document.querySelectorAll(".key")
+keys.forEach(key=> key.addEventListener("transitionend", removeTransition))
